d46faa476318c28f484407af80c41c65c1301f28
RemkovdZee <vanderzee@ice-up.eu>
2020-09-06 15:23:26 +0000

    made a calculator that kan add numbers and gives an error when an other data form is used

---
+++ spec/calcSpec.js
@@ -0,0 +1,24 @@
+describe("Calculator", function() {
+
+    beforeEach(function(){    
+    calc = new Calculator;
+    });
+
+    describe("addition", function(){
+        it("should return 42", function(){
+            calc.add(20);
+            calc.add(22);
+            expect(calc.value).toBe(42)
+        })
+        it("should return 26", function(){
+            calc.add(9);
+            calc.add(17);
+        expect(calc.value).toBe(26)
+        })
+        it("should return an error if we don't supply two numbers", function(){
+        spyOn(window, "alert");    
+        calc.add("Hitchhikers");
+        expect(window.alert).toHaveBeenCalledWith("Error!")
+        })
+    })
+})
